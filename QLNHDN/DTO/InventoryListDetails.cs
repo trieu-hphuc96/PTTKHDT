5c5f753384a428c422577ceda38cd2f448cd1d1b
Trieste96 <truongminhtriet96@gmail.com>
2017-04-04 13:50:13 +0700

    Revert "Revert "Merge 4/4/2017""
    This reverts commit 5af9d5491416c47a22b98944116cceb47dd834f1.

---
+++ QLNHDN/DTO/InventoryListDetails.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class InventoryListDetails
+    {
+        private int maphieu;
+        private int manl;
+        private int soluong;
+
+        public int Maphieu
+        {
+            get
+            {
+                return maphieu;
+            }
+
+            set
+            {
+                maphieu = value;
+            }
+        }
+
+        public int Manl
+        {
+            get
+            {
+                return manl;
+            }
+
+            set
+            {
+                manl = value;
+            }
+        }
+
+        public int Soluong
+        {
+            get
+            {
+                return soluong;
+            }
+
+            set
+            {
+                soluong = value;
+            }
+        }
+    }
+}
